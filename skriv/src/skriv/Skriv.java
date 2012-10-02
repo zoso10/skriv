9319aa8545f0351abf5190a204c731a6e1f5ad13
Tyler Ewing <tewing10@gmail.com>
2012-10-02 06:53:50 -0400

    Cleaning up a little bit, NB puts a lot of junk in the beginning

--- skriv/src/skriv/Skriv.java
+++ skriv/src/skriv/Skriv.java
@@ -1,12 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package skriv;
 
 /**
  *
- * @author Tyler
+ * @author Tyler Ewing, David Ecker, Jacob Shaffer, Matthew Swenson
+ * I'd like to keep everything well documented as we go along
+ * I think it would help each other understand everyone's code
+ * Be verbose, every little bit of information is helpful
  */
 public class Skriv {
 
@@ -14,6 +13,6 @@ public class Skriv {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
