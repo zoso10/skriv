441b2f419994ec7839545af52e0aad6ae5f14eba
Tyler Ewing <tewing10@gmail.com>
2012-11-13 12:52:38 -0500

    Started working on the Writing Zoom Box

---
+++ skriv/src/test/writing/box/View.java
@@ -0,0 +1,16 @@
+package test.writing.box;
+
+import javax.swing.JFrame;
+
+
+public class View extends JFrame{
+    
+    public View(){
+        super();
+        init();
+    }
+    
+    private void init(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
