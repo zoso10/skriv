4b448576096a5ab59e128eac4cf3dc683cabd7df
Tyler Ewing <tewing10@gmail.com>
2012-11-15 15:44:40 -0500

    Implemented Listener methods in test.writing.box.Controller

--- skriv/src/test/writing/box/Controller.java
+++ skriv/src/test/writing/box/Controller.java
@@ -8,12 +8,15 @@ import javax.swing.SwingUtilities;
 import classes.View;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 /**
  *
  * @author Tyler_Ewing
  */
-public class Controller implements ActionListener{
+public class Controller implements ActionListener, MouseListener, MouseMotionListener{
     
     private Model model;
     private View view;
@@ -24,16 +27,55 @@ public class Controller implements ActionListener{
         view.setVisible(true);
     }
     
-    // Override for Button Listeners
+    // IMplementation for Button Listeners
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println(String.format("%s button pressed!!", e.getActionCommand()));
+    }
+    
+    // Implementation for Mouse Listeners
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // Probably won't have functionality
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // Add Point and see if it's a new Word
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // Add Point to denote the pen has been lifted
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // Probably won't have functionality
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // Probably won't have functionality
+    }
+
+    // Implementation for Mouse Motion Listener
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        
     }
     
     private void addButtonListeners(){
         view.addButtonListeners(this);
     }
     
+    
+    // Launch the application
     public static void launch(){
         SwingUtilities.invokeLater(new Runnable(){
             @Override
@@ -44,6 +86,7 @@ public class Controller implements ActionListener{
         });
     }
     
+    // MAIN
     public static void main(String[] args){
         launch();
     }
