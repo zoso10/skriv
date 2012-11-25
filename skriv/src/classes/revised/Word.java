12223f81004f4005c956d9f1193f61af376e99f5
Tyler Ewing <tewing10@gmail.com>
2012-11-25 00:41:58 -0500

    Rewrite of the Word class

---
+++ skriv/src/classes/revised/Word.java
@@ -0,0 +1,60 @@
+package classes.revised;
+
+import classes.SmartPoint;
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Word {
+    
+    // Trying to keep this Word class independent
+    
+    private List<SmartPoint> points;
+    private Color c;
+    private Color hc; // Highlighted Color
+    private boolean isHighlighted;
+    private Rectangle boundingBox;
+    private int length;
+    private int height;
+    // I'm still trying to decide if I need this or even how to calculate it
+    //private int row;
+    // I don't think this will be needed since location isn't important
+    //private int top, bottom, left, right;
+    
+    
+    // I may can this default constructor
+    public Word(){
+        isHighlighted = false;
+        hc = Color.YELLOW;
+        c = Color.BLACK;
+        boundingBox = new Rectangle();
+        length = 0;
+        height = 0;
+        points = new ArrayList<SmartPoint>();
+    }
+    
+    public Word(SmartPoint p){
+        this();
+        this.add(p);
+    }
+    
+    public void add(SmartPoint p){
+        points.add(p);
+        boundingBox.add(p.getPoint());
+        length = boundingBox.width;
+        height = boundingBox.height;
+    }
+    
+    public int getLength(){
+        return length;
+    }
+    
+    public int getHeight(){
+        return height;
+    }
+    
+    public boolean contains(SmartPoint p){
+        return boundingBox.contains(p.getPoint());
+    }
+}
