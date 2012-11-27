a1e8c5ae736ea8935dabd3e7b1e6ff9842cae61a
David Ecker <dcecker@gmail.com>
2012-11-26 22:50:19 -0500

    Moar JFX

---
+++ skriv/src/javafxapplication2/SampleController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+/**
+ *
+ * @author davidecker
+ */
+public class SampleController implements Initializable {
+    
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
