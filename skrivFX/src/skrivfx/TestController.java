57e9ec66135336d144cf301778ab3a2d94ad14a3
Tyler Ewing <tewing10@gmail.com>
2012-11-29 12:30:09 -0500

    Staying hacking away at the package issues

---
+++ skrivFX/src/skrivfx/TestController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skrivfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Tyler
+ */
+public class TestController implements Initializable{
+    
+    @FXML
+    private void handleButtonAction(ActionEvent e){
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        // stuff, maybe...
+    }
+}
