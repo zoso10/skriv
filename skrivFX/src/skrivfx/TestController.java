454dfb172dd6433082055071a9ed3bfba42aadb2
David Ecker <dcecker@gmail.com>
2012-11-30 22:41:57 -0500

    added button listeners

--- skrivFX/src/skrivfx/TestController.java
+++ skrivFX/src/skrivfx/TestController.java
@@ -11,6 +11,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
 import javafx.scene.control.TitledPane;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
@@ -25,42 +26,82 @@ public class TestController implements Initializable{
     
     @FXML
     private ToggleButton menuButton;
-    
-    @FXML
-    private TitledPane openPane;
-    
     @FXML
     private AnchorPane menuPane;
+    @FXML
+    private ToggleButton writeButton;
+    @FXML
+    private Button openButton;
+    @FXML
+    private Button newButton;
+    @FXML
+    private Button saveButton;
+    @FXML
+    private Button closeButton;
     
     @FXML
-    private void handleButtonAction(ActionEvent e){
-        System.out.println("dont touch me there");
+    private void handleWriteButtonAction(ActionEvent e){
+        if (writeButton.isSelected()){
+            System.out.println("write button on");
+        }
+        else{
+            System.out.println("write button off");
+        } 
     }
     
-    @Override // This method is called by the FXMLLoader when initialization is complete
-    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-        assert menuButton != null : "fx:id=\"menuButton\" was not injected: check your FXML file 'jfxGUI.fxml'.";
-
-        // initialize your logic here: all @FXML variables will have been injected
-        menuButton.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                if (menuButton.isSelected()){
-                    //menu.setOpacity(1.0);
-                    menuPane.setVisible(true);
-                }
-                else{
-                    //menu.setOpacity(0.0);
-                    menuPane.setVisible(false);
-                }
-            }
-        });     
-
+    @FXML
+    private void handleMenuButtonAction(){
+        if (menuButton.isSelected()){
+            menuPane.setVisible(true);
+        }
+        else{
+            menuPane.setVisible(false);
+        }
     }
     
-//    @Override
-//    public void initialize(URL url, ResourceBundle rb){
-//        // stuff, maybe...
+    @FXML
+    private void handleNewButtonAction(){
+        System.out.println("new button pressed");
+    }
+    
+    @FXML
+    private void handleOpenButtonAction(){
+        System.out.println("open button pressed");
+    }
+    
+    @FXML
+    private void handleSaveButtonAction(){
+        System.out.println("save button pressed");
+    }
+    @FXML
+    private void handleCloseButtonAction(){
+        System.out.println("close button pressed");
+    }
+    
+    
+    
+//    @Override // This method is called by the FXMLLoader when initialization is complete
+//    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+//        assert menuButton != null : "fx:id=\"menuButton\" was not injected: check your FXML file 'jfxGUI.fxml'.";
+//
+//        // initialize your logic here: all @FXML variables will have been injected
+//        menuButton.setOnAction(new EventHandler<ActionEvent>() {
+//
+//            @Override
+//            public void handle(ActionEvent event) {
+//                if (menuButton.isSelected()){
+//                    menuPane.setVisible(true);
+//                }
+//                else{
+//                    menuPane.setVisible(false);
+//                }
+//            }
+//        });     
+//
 //    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        // stuff, maybe...
+    }
 }
