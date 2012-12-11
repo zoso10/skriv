a0dcf4819cdc3ba90c4d0a1c3750edae0861e4b5
Jacob <Jacob@Jacob-PC>
2012-12-11 14:20:01 -0500

    added mouse handlers to page but need reference to word list.. maybe it is in the model?

--- skrivFX/src/classes/Page.java
+++ skrivFX/src/classes/Page.java
@@ -4,52 +4,65 @@ import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Tab;
+import javafx.scene.input.MouseEvent;
 
+public class Page extends Tab {
 
-public class Page extends Tab{
-    
     private Canvas canvas;
     private GraphicsContext gc;
     private double curX, curY; // Cursor
     // Maybe...
     // A Page can only have one set of Notes
     //private Notes notes;
-    
-    public Page(){
+
+    public Page() {
         this("Untitled");
     }
-    
-    public Page(String title){
+
+    public Page(String title) {
         super(title);
         super.setContent(canvas);
         curX = 20;
         curY = 20;
     }
-    
-    public void makeCanvas(ReadOnlyDoubleProperty width, ReadOnlyDoubleProperty height){
+
+    public void makeCanvas(ReadOnlyDoubleProperty width, ReadOnlyDoubleProperty height) {
         canvas = new Canvas();
         canvas.widthProperty().bind(width);
         canvas.heightProperty().bind(height);
-        
+
         gc = canvas.getGraphicsContext2D();
     }
-    
-    public void drawWord(Word w){
-        if(canvas.getWidth() - curX < w.getWidth()){
+
+    public void drawWord(Word w) {
+        if (canvas.getWidth() - curX < w.getWidth()) {
             curX = 20;
             curY = curY + w.getHeight() + 20;
         }
-        gc.drawImage(w.getImage(), curX, curY, .65*w.getWidth(), .65*w.getHeight());
+        gc.drawImage(w.getImage(), curX, curY, .65 * w.getWidth(), .65 * w.getHeight());
         curX = curX + 20 + w.getWidth();
     }
-    
-    public void startLine(double x, double y){
+
+    public void startLine(double x, double y) {
         gc.beginPath();
         gc.moveTo(x, y);
     }
-    
-    public void updateLine(double x, double y){
+
+    public void updateLine(double x, double y) {
         gc.lineTo(x, y);
         gc.stroke();
+    }
+
+    public void handle(MouseEvent e) {
+        if (e.getEventType() == MouseEvent.MOUSE_PRESSED) {
+            mousePressedEvent(e);
+        }
+    }
+
+    private void mousePressedEvent(MouseEvent e) {
+        // this is obvious but it needs  reference to the list of words to check
+        // if the x,y pair are within bounds... hmmm
+        double posX = e.getSceneX();
+        double posY = e.getSceneY();
     }
 }
